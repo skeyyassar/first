a3051915c2e3279a0569a8420e9cc64ba944b8b5
skeyyassar <skey.yassarkhokhar@gmail.com>
2023-03-01 17:38:07 +0530

    tail wind css

---
+++ html/TailwindCss/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      keyframes:{
+        yasar:{
+          '0%': {transform: 'translateX(0)'},
+          '50%': {transform: 'translateX(-100%)'},
+          '75%':{transform: 'translateX(-200%)'},
+          '100%':{transform: 'translateX(-300%)'}
+        },
+      },
+      animation:{
+        'slider-animation':'yasar 16s infinite'
+      },
+    },
+  },
+  plugins: [],
+
+}
